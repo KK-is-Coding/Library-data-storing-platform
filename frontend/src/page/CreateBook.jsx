92da0385a9bf5c01b77c7222b5ff3cc571369104
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 02:25:11 +0530

    Update CreateBook.jsx

--- frontend/src/page/CreateBook.jsx
+++ frontend/src/page/CreateBook.jsx
@@ -22,7 +22,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('library-data-storing-platform-api.vercel.app/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully !!', { variant: 'success' });
