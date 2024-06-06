f6ff8440724ff4cff46321ae1334eb3c0d4ea3bc
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 02:23:45 +0530

    Update EditBook.jsx

--- frontend/src/page/EditBook.jsx
+++ frontend/src/page/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`library-data-storing-platform-api.vercel.app/book/${id}`)
+    axios.get(`library-data-storing-platform-api.vercel.app/books/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`library-data-storing-platform-api.vercel.app/book/${id}`, data)
+      .put(`library-data-storing-platform-api.vercel.app/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully !!', { variant: 'success' });
