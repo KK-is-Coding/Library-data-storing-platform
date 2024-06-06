d77d3fde04f0c07205be735336b7f0b2c3c4bf71
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 02:30:46 +0530

    Update DeleteBook.jsx

--- frontend/src/page/DeleteBook.jsx
+++ frontend/src/page/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`library-data-storing-platform-api.vercel.app/books/${id}`)
+      .delete(`https://library-data-storing-platform-api.vercel.app/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully !!', { variant: 'success' });
