7826c0a48830e87e45875b50cdd7ceadd5e4a58a
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 15:38:41 +0530

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-// export const mongoDBURL = "mongodb+srv://root:kkusingroot@bookstore-mern.1ozwgw7.mongodb.net/book-collection?retryWrites=true&w=majority"
-export const mongoDBURL = "mongodb+srv://root:kkusingroot@bookstore-mern.1ozwgw7.mongodb.net/book-collection?retryWrites=true&w=majority&appName=bookstore-MERN"
