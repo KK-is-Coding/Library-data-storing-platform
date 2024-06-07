c27d5f07627d4edb402ebeffeb95cc931960f3b6
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 20:16:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(
     cors({
         origin: "https://library-data-storing-platform-frontend.vercel.app",
         methods: ['GET', 'POST', 'DELETE', 'PUT'],
-        allowedHeaders: ['Content-Type'],
+        allowedHeaders: ['Origin', 'Content-Type', 'Accept'],
         credentials: true,
     })
 )
