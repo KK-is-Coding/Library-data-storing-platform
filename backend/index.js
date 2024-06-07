ee1f551fe8a3095dc826230f614e989c1c0b820c
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 11:33:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,17 @@ app.use(express.json())
 
 // Middlewares for handling CORS policy
 // Option 1: Allow all origins with default of cors(*) 
-app.use(cors())
+// app.use(cors())
 
 // Option 2: Allow custom origins
-// app.use(
-//     cors({
-//         origin: "https://library-data-storing-platform-frontend.vercel.app",
-//         methods: ['GET', 'POST', 'DELETE', 'PUT'],
-//         allowedHeaders: ['Content-Type'],
-//     })
-// )
+app.use(
+    cors({
+        origin: "https://library-data-storing-platform-frontend.vercel.app",
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: ['Content-Type'],
+        withCredentials: true,
+    })
+)
 
 app.get('/', (req, res) => {
     console.log("request passed...")
