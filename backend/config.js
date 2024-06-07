826441a32e6fbfa768e3da9fe5ea84b783fe9f4c
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2024-06-07 14:36:11 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb+srv://root:kkusingroot@bookstore-mern.1ozwgw7.mongodb.net/book-collection?retryWrites=true&w=majority"
-// export const mongoDBURL = "mongodb+srv://root:kkusingroot@bookstore-mern.1ozwgw7.mongodb.net/book-collection?retryWrites=true&w=majority&appName=bookstore-MERN"
+// export const mongoDBURL = "mongodb+srv://root:kkusingroot@bookstore-mern.1ozwgw7.mongodb.net/book-collection?retryWrites=true&w=majority"
+export const mongoDBURL = "mongodb+srv://root:kkusingroot@bookstore-mern.1ozwgw7.mongodb.net/book-collection?retryWrites=true&w=majority&appName=bookstore-MERN"
